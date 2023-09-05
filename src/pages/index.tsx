d9b928e79d6d5d90a429a38f638bdd2968d2bb7e
vietnguyen-dev <77704810+vietnguyen-dev@users.noreply.github.com>
2023-09-05 16:12:49 -0700

    navbar done

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,8 +8,8 @@ const inter = Inter({ subsets: ["latin"] });
 export default function Home() {
   return (
     <main className={`${inter.className}`}>
-      <nav className="flex justify-between px-12 py-6">
-        <Link href="/">
+      <nav className="navbar px-6 py-3 md:px-12 md:py-6 md:justify-between">
+        <Link href="/" className="flex-1 md:flex-none">
           <Image
             src={Logo}
             width={100}
@@ -17,7 +17,7 @@ export default function Home() {
             alt="Picture of the author"
           />
         </Link>
-        <div className="flex">
+        <div className="hidden md:block">
           <Link href="/how-it-works" className="mr-3 align-middle">
             How it Works
           </Link>
@@ -28,9 +28,26 @@ export default function Home() {
             Testimonials
           </Link>
         </div>
-        <button className="btn btn-primary">
-          <Link href="/booking">Book Now</Link>
-        </button>
+        <div className="flex-none">
+          <button className="btn btn-primary flex-none ">
+            <Link href="/booking">Book Now</Link>
+          </button>
+          <button className="btn btn-square btn-ghost ml-3 md:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              className="inline-block w-5 h-5 stroke-current"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h16M4 18h16"
+              ></path>
+            </svg>
+          </button>
+        </div>
       </nav>
     </main>
   );
