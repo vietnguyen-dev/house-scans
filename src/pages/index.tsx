5617fee9c00fd85022512815aba732fd439309f8
vietnguyen-dev <77704810+vietnguyen-dev@users.noreply.github.com>
2023-09-05 15:50:31 -0700

    working on top navigation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,37 @@
 import { Inter } from "next/font/google";
+import Image from "next/image";
+import Link from "next/link";
+import Logo from "../../public/next.svg";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <h1>Hello World</h1>
+    <main className={`${inter.className}`}>
+      <nav className="flex justify-between px-12 py-6">
+        <Link href="/">
+          <Image
+            src={Logo}
+            width={100}
+            height={100}
+            alt="Picture of the author"
+          />
+        </Link>
+        <div className="flex">
+          <Link href="/how-it-works" className="mr-3 align-middle">
+            How it Works
+          </Link>
+          <Link href="/faq" className="mr-3">
+            FAQs
+          </Link>
+          <Link href="/testimonials" className="mr-3">
+            Testimonials
+          </Link>
+        </div>
+        <button className="btn btn-primary">
+          <Link href="/booking">Book Now</Link>
+        </button>
+      </nav>
     </main>
   );
 }
