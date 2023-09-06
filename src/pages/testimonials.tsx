3995f132e21b22fe7491a1d1fbf86ff208ee5bd9
vietnguyen-dev <77704810+vietnguyen-dev@users.noreply.github.com>
2023-09-06 15:59:19 -0700

    does strapi work in prod?

--- src/pages/testimonials.tsx
+++ src/pages/testimonials.tsx
@@ -1,11 +1,24 @@
 import { Inter } from "next/font/google";
+import axios from "axios";
 
 import { Navbar } from "@/components/navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
-function Rating() {
-  const inputElements = Array.from({ length: 5 }, (_, index) => (
+interface iTestimonial {
+  id: number;
+  attributes: {
+    createdAt: Date;
+    name: string;
+    publishedAt: Date;
+    rating: number;
+    review: string;
+    updatedAt: Date;
+  };
+}
+
+function Rating(props: { number: number }) {
+  const inputElements = Array.from({ length: props.number }, (_, index) => (
     <input
       key={index}
       type="radio"
@@ -19,26 +32,46 @@ function Rating() {
   return <div>{inputElements}</div>;
 }
 
-export default function Testimonials() {
+export default function Testimonials({
+  testimonials,
+}: {
+  testimonials: iTestimonial[];
+}) {
+  console.log(testimonials);
   return (
     <main className={`${inter.className}`}>
       <Navbar />
       <h1 className="text-center mt-3 font-bold text-3xl">Testimonials</h1>
-      {/* <div>
-        <div className="card w-96 bg-base-100 shadow-xl">
-          <div className="card-body">
-            <h2 className="card-title">George Wallen</h2>
-            <div className="rating">
-              <Rating />
+      <div className="px-6 flex justify-center gap-4">
+        {testimonials.map((testimonial) => (
+          <div className="card w-72 bg-base-100 shadow-xl" key={testimonial.id}>
+            <div className="card-body">
+              <h2 className="card-title">{testimonial.attributes.name}</h2>
+              <div className="rating">
+                <Rating number={testimonial.attributes.rating} />
+              </div>
+              <p>{testimonial.attributes.review}</p>
+              {/* <div className="card-actions justify-end">
+                <button className="btn btn-primary">Buy Now</button>
+              </div> */}
             </div>
-            <p>
-              If a dog chews shoes whose shoes does he choose? If a dog chews
-              shoes whose shoes does he choose? If a dog chews shoes whose shoes
-              does he choose?
-            </p>
           </div>
-        </div>
-      </div> */}
+        ))}
+      </div>
     </main>
   );
+}
+
+export async function getStaticProps() {
+  try {
+    const data = await axios.get(`${process.env.API_URL}/testimonials`);
+    const testimonials = data.data.data;
+    return {
+      props: {
+        testimonials,
+      },
+    };
+  } catch (err) {
+    console.error(err);
+  }
 }
